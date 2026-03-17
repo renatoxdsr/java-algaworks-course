c1b4a85c91b5d43cbcbf78e29ec627722158b6ee
Renato Xavier <59875607+renatoxdsr@users.noreply.github.com>
2026-03-17 22:21:42 +0000

    Create aula-Tipo-Texto-String.java

---
+++ aula-Tipo-Texto-String.java
@@ -0,0 +1,18 @@
+package algaworks;
+import java.util.Scanner;
+
+
+public class tipoTexto {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String nome = scanner.nextLine();
+		
+		System.out.println("Ola" + nome + "!");
+		
+		scanner.close();
+		
+		
+	}
+}
